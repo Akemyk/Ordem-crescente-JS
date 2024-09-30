78f5d7fed962718ae485c7662415f6d9619bb653
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 14:02:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let valor1 = Number(prompt("Digite um número")) 
+let valor2 = Number(prompt("Digite outro número"))
+if (valor1<valor2) {
+    alert("Os valores em ordem crescente são: "+ valor1+ " e "+valor2)
+}else{
+    alert("Os valores em ordem crescente são: " + valor2 + " e " + valor1)
+}
